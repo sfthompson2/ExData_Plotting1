158e1cf93fc74630521eed1c5f6414c0dee3064d
sfthompson2 <sfthompson2@StephansonsiMac.fios-router.home>
2016-01-09 17:01:54 -0500

    Plot 2
    Plot 2

---
+++ plot2.r
@@ -0,0 +1,16 @@
+#Loading the data
+mydata <- read.table ("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
+
+mydata$Time <- paste(mydata$Date, mydata$Time,sep = " ")
+
+mydata$Date <- as.Date(mydata$Date, format = "%d/%m/%Y")
+mydata$Time <- strptime(mydata$Time, format = "%d/%m/%Y %H:%M:%S")
+
+sset <- subset(mydata, as.Date(mydata$Date, "%d/%m/%Y") >= "2007-02-01" & as.Date(mydata$Date, "%d/%m/%Y") <= "2007-02-02")
+
+#Making Plots - #2
+png (file="plot2.png", width=480, height=480)
+
+with(sset, plot(Time, Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)"))
+
+dev.off()
